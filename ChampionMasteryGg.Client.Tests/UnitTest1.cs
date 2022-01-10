f9baa0d1e7dbf8f8a248ba8eab1988e37b4f473f
Mikael Dúi Bolinder <github@bolinder.uk>
2022-01-10 07:06:05 +0100

    Moves some files (#8)
    * Moved the files
    
    * Should deploy feature branches
    
    * Update dotnet-release.yml
    
    * Update dotnet-release.yml

---
+++ ChampionMasteryGg.Client.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ChampionMasteryGg.Client.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void ConstructClient()
+        {
+            _ = new ChampionMasteryGgClient();
+        }
+    }
+}
