287a9502dcb3a6b7be66f7f2468e6676ad874669
Mikael Dúi Bolinder <github@bolinder.uk>
2022-02-05 07:56:37 +0100

    Custom converter for ChampionHighscoresDictionary

--- ChampionMasteryGg.Client/Models/ChampionHighscoresDictionary.cs
+++ ChampionMasteryGg.Client/Models/ChampionHighscoresDictionary.cs
@@ -1,13 +1,37 @@
 ﻿using System.Collections.ObjectModel;
 using System.Diagnostics;
+using System.Reflection;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 
 namespace ChampionMasteryGg
 {
-    [DebuggerDisplay("Champions = {Count}")]
+    [JsonConverter(typeof(ChampionHighscoresDictionaryConverter)), DebuggerDisplay("Champions = {Count}")]
     public class ChampionHighscoresDictionary : ChampionMasteryGgDictionary<Champion, HighscoresCollection<PointsHighscore>>
     {
         internal ChampionHighscoresDictionary(IDictionary<Champion, HighscoresCollection<PointsHighscore>> dictionary) : base(dictionary)
         {
         }
+    }
+
+    public class ChampionHighscoresDictionaryConverter : JsonConverter<ChampionHighscoresDictionary>
+    {
+        public override ChampionHighscoresDictionary? Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+        {
+            var dict = JsonSerializer.Deserialize<Dictionary<int, HighscoresCollection<PointsHighscore>>>(ref reader, options: options);
+
+            if (dict == null)
+                return null;
+
+            return (ChampionHighscoresDictionary)Activator.CreateInstance(
+                typeToConvert,
+                BindingFlags.Instance | BindingFlags.Public,
+                binder: null,
+                args: new object[] { dict.ToDictionary(kvp => new Champion { Id = kvp.Key }, kvp => kvp.Value) },
+                culture: null);
+        }
+
+        public override void Write(Utf8JsonWriter writer, ChampionHighscoresDictionary dictionary, JsonSerializerOptions options) =>
+            JsonSerializer.Serialize(writer, dictionary?.ToDictionary(kvp => kvp.Key.Id, kvp => kvp.Value), options);
     }
 }
